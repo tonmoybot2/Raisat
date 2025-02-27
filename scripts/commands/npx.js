b1922d9a98093e5f6677b3174b9671f516544c30
tonmoybot2 <fftonmoy67@gmail.com>
2025-02-28 00:52:28 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
+	if (body.indexOf("🤨")==0 || body.indexOf("😱")==0 || body.indexOf("🙄")==0 || body.indexOf("😒")==0) {
 		var msg = {
 				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
 				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
